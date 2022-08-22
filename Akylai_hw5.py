4fbf10fb85e32e5d0eda47a0fa84ba476bc52d6d
Akylai Syd <110735537+kylaiii@users.noreply.github.com>
2022-08-22 17:15:20 +0600

    Add files via upload

---
+++ Akylai_hw5.py
@@ -0,0 +1,16 @@
+import re
+class ValidCarNumber:
+    number = input("Введите номер по шаблону (01KG777ABC):")
+    def __int__(self, number):
+        self.number = number
+
+    def is_valid(self):
+        is_valid = re.search(r"0[0-9]KG([0-9]{3})([A-Z]{3})", self.number)
+        try:
+            if self.number[0:is_valid.end()] == self.number:
+              print("Номер валидный!")
+        except:
+           print("Номер не валидный!")
+
+carnumber = ValidCarNumber()
+carnumber.is_valid()
